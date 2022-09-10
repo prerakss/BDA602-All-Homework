66fc85c173f99598c5ab7a0ef33c479567b89634
prerakss <89816387+prerakss@users.noreply.github.com>
2022-09-09 20:52:42 -0700

    requested changes are made

--- iris.py
+++ iris.py
@@ -29,11 +29,14 @@ print("25% Quantile: ", np.quantile(arr_iris, q=0.25, axis=0))
 print("50% Quantile: ", np.quantile(arr_iris, q=0.5, axis=0))
 print("75% Quantile: ", np.quantile(arr_iris, q=0.75, axis=0))
 
+# Scatter plot
 fig_scatter = px.scatter(
     df_iris, x="sepal width", y="sepal length", color="class", symbol="class"
 )
-fig_scatter.show()
+# fig_scatter.show()
+fig_scatter.write_html(file="fig_scatter.html", include_plotlyjs="cdn")
 
+# Pie plot
 fig = px.pie(
     df_iris,
     values="petal width",
@@ -41,9 +44,12 @@ fig = px.pie(
     color_discrete_sequence=px.colors.sequential.RdBu,
 )
 fig.show()
+fig.write_html(file="fig.html", include_plotlyjs="cdn")
 
+# Violin plot
 fig_violin = px.violin(df_iris, box=True, y="sepal width", color="class")
-fig_violin.show()
+# fig_violin.show()
+fig_violin.write_html(file="fig_violin.html", include_plotlyjs="cdn")
 
 aggregations = {
     "sepal length": lambda x: np.max(x),
@@ -53,15 +59,21 @@ aggregations = {
 }
 df_iris_grouped = df_iris.groupby("class", as_index=False).agg(aggregations)
 
+# Bar plots
 fig_bar = px.bar(df_iris_grouped, x="class", y="sepal length", color="class")
-fig_bar.show()
+# fig_bar.show()
+fig_scatter.write_html(file="fig_bar.html", include_plotlyjs="cdn")
 
 fig_bar_2 = px.bar(df_iris_grouped, x="sepal width", y="class", color="class")
-fig_bar_2.show()
-
+# fig_bar_2.show()
+fig_scatter.write_html(file="fig_bar_2.html", include_plotlyjs="cdn")
 
 X_orig = df_iris[["sepal length", "sepal width", "petal length", "petal width"]].values
 y = df_iris["class"].values
+
+# Scatter-Matrix plot
+fig_matrix = px.scatter_matrix(df_iris)
+fig_matrix.show()
 
 standard_scaler = StandardScaler()
 standard_scaler.fit(X_orig)
@@ -90,7 +102,10 @@ pipeline = Pipeline(
 
 
 pipeline2 = Pipeline([("StandardScaler", StandardScaler()), ("SVC", SVC())])
+
 pipeline.fit(X_orig, y)
 pipeline2.fit(X_orig, y)
 
+print("Pipeline 1 score", pipeline.score(X, y))
+print("Pipeline 2 score", pipeline2.score(X, y))
 prediction_3 = pipeline.predict(X)
