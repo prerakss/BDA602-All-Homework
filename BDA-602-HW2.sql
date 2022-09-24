b798f8c18be70a85da97a020f34689b8014cd127
prerakss <89816387+prerakss@users.noreply.github.com>
2022-09-23 20:36:45 -0700

    Update BDA-602-HW2.sql

--- BDA-602-HW2.sql
+++ BDA-602-HW2.sql
@@ -1,44 +1,42 @@
-
 #Annual Batting Average
 
+create table annual_avg (
 select
 	bc.batter,
 	year(g.local_date) as year,
-	sum(Hit)/ sum(atBat) as batting_avg
+	round(sum(Hit)/ sum(atBat), 3) as batting_avg
 from
 	batter_counts bc
 left join game g on
 	g.game_id = bc.game_id
 where
-	Hit <> 0
-	and atBat <> 0
+	atBat <> 0
 group by
 	batter,
 	year(g.local_date)
+HAVING
+	year is not null
 order by
-	batter;
-
-
+	batter);
 #Historic Batting Average
 
+create table if not exists historic_avg as (
 select
 	bc.batter,
-	sum(Hit)/ sum(atBat) batting_avg
+	round(sum(Hit)/ sum(atBat), 3) batting_avg
 from
 	batter_counts bc
 left join game g on
 	g.game_id = bc.game_id
 where
-	Hit <> 0
-	and atBat <> 0
+	atBat <> 0
 group by
 	bc.batter
 order by
-	bc.batter;
-
-
+	bc.batter);
 #Rolling Batting Average
 
+create table if not exists rolling_avg as (
 with temp_1 AS(
 select
 	bc.batter,
@@ -51,36 +49,33 @@ from
 left join game g on
 	g.game_id = bc.game_id
 where
-	Hit <> 0
-	and atBat <> 0
+	atBat <> 0
 ),
- 
-temp_2 as (
+	temp_2 as (
 select
-	batter,
-	game_id,
-	days_diff,
-	sum(Hit)/ sum(atBat) as bat_avg
+		batter,
+		game_id,
+		days_diff,
+		sum(Hit)/ sum(atBat) as bat_avg
 from
-	temp_1
+		temp_1
 group by
-	batter,
-	game_id,
-	days_diff 
+		batter,
+		game_id,
+		days_diff 
 )
- 
 select
-	batter,
-	game_id
+		batter,
+		game_id
 ,
-	(
+		(
 	select
-		avg(bat_avg)
+			avg(bat_avg)
 	from
-		temp_2 as t2
+			temp_2 as t2
 	where
-		t2.days_diff between 
+			t2.days_diff between 
 t1.days_diff - 100 and t1.days_diff - 1
 		and t1.batter = t2.batter) as batting_avg
 from
-	temp_2 t1
+		temp_2 t1);
