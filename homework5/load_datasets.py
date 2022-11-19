a554538e5c3c3d2301e15f5f9f3f98f28922c7e3
prerakss <89816387+prerakss@users.noreply.github.com>
2022-11-19 02:18:17 -0800

    hw5 final commit

---
+++ homework5/load_datasets.py
@@ -0,0 +1,101 @@
+import random
+from typing import List
+
+import pandas
+import seaborn
+from sklearn import datasets
+
+TITANIC_PREDICTORS = [
+    "pclass",
+    "sex",
+    "age",
+    "sibsp",
+    "embarked",
+    "parch",
+    "fare",
+    "who",
+    "adult_male",
+    "deck",
+    "embark_town",
+    "alone",
+    "class",
+]
+
+
+def get_test_data_set(data_set_name: str = None) -> (pandas.DataFrame, List[str], str):
+    """Function to load a few test data sets
+
+    :param:
+    data_set_name : string, optional
+        Data set to load
+
+    :return:
+    data_set : :class:`pandas.DataFrame`
+        Tabular data, possibly with some preprocessing applied.
+    predictors :list[str]
+        List of predictor variables
+    response: str
+        Response variable
+    """
+    seaborn_data_sets = ["mpg", "tips", "titanic", "titanic_2"]
+    sklearn_data_sets = ["boston", "diabetes", "breast_cancer"]
+    all_data_sets = seaborn_data_sets + sklearn_data_sets
+
+    if data_set_name is None:
+        data_set_name = random.choice(all_data_sets)
+    else:
+        if data_set_name not in all_data_sets:
+            raise Exception(f"Data set choice not valid: {data_set_name}")
+
+    if data_set_name in seaborn_data_sets:
+        if data_set_name == "mpg":
+            data_set = seaborn.load_dataset(name="mpg").dropna().reset_index()
+            predictors = [
+                "cylinders",
+                "displacement",
+                "horsepower",
+                "weight",
+                "acceleration",
+                "origin",
+                "name",
+            ]
+            response = "mpg"
+        elif data_set_name == "tips":
+            data_set = seaborn.load_dataset(name="tips").dropna().reset_index()
+            predictors = [
+                "total_bill",
+                "sex",
+                "smoker",
+                "day",
+                "time",
+                "size",
+            ]
+            response = "tip"
+        elif data_set_name == "titanic":
+            data_set = seaborn.load_dataset(name="titanic").dropna()
+            predictors = TITANIC_PREDICTORS
+            response = "survived"
+        elif data_set_name == "titanic_2":
+            data_set = seaborn.load_dataset(name="titanic").dropna()
+            predictors = TITANIC_PREDICTORS
+            response = "alive"
+    elif data_set_name in sklearn_data_sets:
+        if data_set_name == "boston":
+            print("lol")
+            """data = datasets.load_boston()
+            data_set = pandas.DataFrame(data.data, columns=data.feature_names)
+            data_set["CHAS"] = data_set["CHAS"].astype(str)"""
+        elif data_set_name == "diabetes":
+            data = datasets.load_diabetes()
+            data_set = pandas.DataFrame(data.data, columns=data.feature_names)
+            data_set["gender"] = ["1" if i > 0 else "0" for i in data_set["sex"]]
+        elif data_set_name == "breast_cancer":
+            data = datasets.load_breast_cancer()
+            data_set = pandas.DataFrame(data.data, columns=data.feature_names)
+
+        data_set["target"] = data.target
+        predictors = data.feature_names
+        response = "target"
+
+    print(f"Data set selected: {data_set_name}")
+    return data_set, predictors, response
